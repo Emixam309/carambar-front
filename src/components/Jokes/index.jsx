57d30ad7de1391d655b601ed02a436208cd69e1c
Maxime Croisier <crmaxime@gmail.com>
2023-06-19 16:31:54 +0200

    add jokes

---
+++ src/components/Jokes/index.jsx
@@ -0,0 +1,29 @@
+import { useEffect, useState } from 'react'
+import { fetchApi } from '../../utils/fetchUtils'
+import "./styles.css"
+
+const Index = () => {
+  const [joke, setJoke] = useState({})
+
+  useEffect(() => {
+    getJoke()
+  }, [])
+
+  const getJoke = () => {
+    fetchApi('/jokes/random')
+      .then(data => setJoke(data))
+  }
+
+  return (
+    <>
+      <h2>Voici une blague :</h2>
+      <div className="joke">
+        <p>{joke?.joke}</p>
+        <p>{joke?.answer}</p>
+      </div>
+      <button className="joke button" onClick={getJoke}>Afficher une nouvelle blague</button>
+    </>
+  )
+}
+
+export default Index
